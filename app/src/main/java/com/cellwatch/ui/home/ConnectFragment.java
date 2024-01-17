6f40345379b494c1f88227a8aa45b86754a76951
Robertson, Scott Lee <scott.robertson@imtc.gatech.edu>
2024-01-17 11:12:14 -0500

    Update ConnectFragment.java

--- app/src/main/java/com/cellwatch/ui/home/ConnectFragment.java
+++ app/src/main/java/com/cellwatch/ui/home/ConnectFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.cellwatch.R;
+
 public class ConnectFragment extends Fragment {
 
     public ConnectFragment() {
