04c513ae46b512360211b53e5379b352cdf42355
Jason Cox <me@jasoncarloscox.com>
2024-02-13 08:46:49 -0500

    Restore functional measurment UI
    It'll still need plenty of improvements, but at least you can see that a
    measurement is being taken.

--- app/src/main/java/edu/gatech/cc/cellwatch/ui/map/MeasureFragment.kt
+++ app/src/main/java/edu/gatech/cc/cellwatch/ui/map/MeasureFragment.kt
@@ -8,27 +8,30 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import edu.gatech.cc.cellwatch.R
+import edu.gatech.cc.cellwatch.core.util.Log
+import edu.gatech.cc.cellwatch.data.model.Measurement
+import edu.gatech.cc.cellwatch.databinding.FragmentMeasureBinding
+import edu.gatech.cc.cellwatch.domain.fcc.MeasurementManager
+import edu.gatech.cc.cellwatch.domain.fcc.ThroughputMetrics
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 class MeasureFragment : Fragment() {
+    private val TAG = this::class.simpleName
     private var progress = 0
-    var buttonIncrement: Button? = null
-    var buttonDecrement: Button? = null
-    var progressBar: ProgressBar? = null
-    var textView: TextView? = null
+    private lateinit var binding: FragmentMeasureBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_measurement, container, false)
-        buttonDecrement = rootView.findViewById<View>(R.id.button_decr) as Button
-        buttonIncrement = rootView.findViewById<View>(R.id.button_incr) as Button
-        progressBar = rootView.findViewById<View>(R.id.progress_bar) as ProgressBar
-        textView = rootView.findViewById<View>(R.id.text_view_progress) as TextView
+        binding = FragmentMeasureBinding.inflate(inflater, container, false)
 
         // when clicked on buttonIncrement progress is increased by 10%
-        buttonIncrement!!.setOnClickListener { // if progress is less than or equal
+        binding.buttonIncr.setOnClickListener { // if progress is less than or equal
             // to 90% then only it can be increased
             if (progress <= 90) {
                 progress += 10
@@ -37,20 +40,93 @@ class MeasureFragment : Fragment() {
         }
 
         // when clicked on buttonIncrement progress is decreased by 10%
-        buttonDecrement!!.setOnClickListener { // If progress is greater than
+        binding.buttonDecr.setOnClickListener { // If progress is greater than
             // 10% then only it can be decreased
             if (progress >= 10) {
                 progress -= 10
                 updateProgressBar()
             }
         }
-        return rootView
+
+        binding.buttonMeasure.setOnClickListener {
+            runTestSequence()
+        }
+
+        return binding.root
     }
 
     // updateProgressBar() method sets
     // the progress of ProgressBar in text
     private fun updateProgressBar() {
-        progressBar!!.progress = progress
-        textView!!.text = progress.toString()
+        binding.progressBar.progress = progress
+        binding.textViewProgress.text = progress.toString()
+    }
+
+    private fun runTestSequence() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            binding.latencyContent.text = ""
+            binding.downloadContent.text = ""
+            binding.uploadContent.text = ""
+
+            try {
+                MeasurementManager.runTestSequence(
+                    { binding.textStatus.text = "finding server..." },
+                    { r -> binding.textStatus.text = "found server $r" },
+                    { handleLatencyStart() },
+                    { handleLatencyComplete(it) },
+                    { handleDownloadStart() },
+                    { handleThroughputComplete(binding.downloadContent, it) },
+                    { handleUploadStart() },
+                    { handleThroughputComplete(binding.uploadContent, it) },
+                )
+                binding.textStatus.text = "measurement complete"
+            } catch (e: Exception) {
+                Log.e(TAG, "unexpected error running test sequence", e)
+
+                if (binding.textStatus.text == "" || binding.textStatus.text == "finding server...") {
+                    binding.textStatus.text == "failed to find server"
+                } else {
+                    binding.textStatus.text == "error running measurement"
+                }
+
+                if (binding.latencyContent.text == "" || binding.latencyContent.text == "running...") {
+                    binding.latencyContent.text = "failed"
+                }
+
+                if (binding.downloadContent.text == "" || binding.downloadContent.text == "running...") {
+                    binding.downloadContent.text = "failed"
+                }
+
+                if (binding.uploadContent.text == "" || binding.uploadContent.text == "running...") {
+                    binding.uploadContent.text = "failed"
+                }
+            }
+        }
+    }
+
+    private fun handleLatencyStart() {
+        binding.textStatus.text = "measuring latency"
+        binding.latencyContent.text = "running..."
+    }
+
+    private fun handleLatencyComplete(m: Measurement) {
+        val rttMillis = (m.latencyData?.rtt ?: 0) / 1e3
+        binding.latencyContent.text = if (m.success == true) "$rttMillis ms" else "failed"
+    }
+
+    private fun handleDownloadStart() {
+        binding.textStatus.text = "measuring download speed"
+        binding.downloadContent.text = "running..."
+    }
+
+    private fun handleUploadStart() {
+        binding.textStatus.text = "measuring upload speed"
+        binding.uploadContent.text = "running..."
+    }
+
+    private fun handleThroughputComplete(content: TextView, m: Measurement) {
+        val activeMetrics = ThroughputMetrics(m.uploadDownloadData?.bytes ?: 0, m.uploadDownloadData?.duration ?: 0)
+        val speedMbps = (activeMetrics.bytesPerSec * 8 / 1e6).roundToInt()
+        content.text = if (m.success == true) "$speedMbps Mbps" else "failed"
     }
 }
