380b6d46a42de7dad8e1c584b600a06fd048284a
Jason Cox <me@jasoncarloscox.com>
2024-02-29 18:54:46 -0500

    Make CountableSocket work with TLS again
    Use Conscrypt to magically fix the CountableSocket. This code used to be
    in the MainActivity but got lost somewhere along the way. Bring it back.
    
    Fixes #13.

--- app/src/main/java/edu/gatech/cc/cellwatch/CellWatchApp.kt
+++ app/src/main/java/edu/gatech/cc/cellwatch/CellWatchApp.kt
@@ -7,10 +7,19 @@ import edu.gatech.cc.cellwatch.data.local.CellWatchDatabase
 import edu.gatech.cc.cellwatch.data.core.repositories.MeasurementRepository
 import edu.gatech.cc.cellwatch.data.datastore.LocalDataStore
 import edu.gatech.cc.cellwatch.data.network.NetworkMeasurementDatasource
+import org.conscrypt.Conscrypt
+import java.security.Security
 
 class CellWatchApp : Application() {
     init {
         instance = this
+
+        // Using Conscrypt somehow makes the CountableSocket work with TLS sockets --
+        // it doesn't otherwise. I found a project on GitHub trying to count socket bytes
+        // (https://github.com/dave-r12/okhttp-byte-counter) and then found a linked
+        // issue (https://github.com/google/conscrypt/issues/65) that suggests Conscrypt
+        // might eventually solve the problem but hasn't yet. I guess it has now...
+        Security.insertProviderAt(Conscrypt.newProvider(), 1)
     }
 
     companion object {
