eca92bec4075f84d7194fc6733daf64537cab423
Jason Cox <me@jasoncarloscox.com>
2024-03-13 12:12:26 -0400

    Show device ID and app version in settings
    These bits of metadata can be useful for triaging bugs or finding
    measurement data in the backend. Show them discreetly at the bottom of
    the settings page, and copy them to the clipboard when tapped.

--- app/src/main/java/edu/gatech/cc/cellwatch/ui/map/SettingsFragment.kt
+++ app/src/main/java/edu/gatech/cc/cellwatch/ui/map/SettingsFragment.kt
@@ -1,17 +1,28 @@
 package edu.gatech.cc.cellwatch.ui.map
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import edu.gatech.cc.cellwatch.BuildConfig
+import edu.gatech.cc.cellwatch.CellWatchApp
 import edu.gatech.cc.cellwatch.R
+import edu.gatech.cc.cellwatch.core.util.Log
 import edu.gatech.cc.cellwatch.databinding.FragmentSettingsBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class SettingsFragment : Fragment() {
+    private val TAG = this::class.simpleName
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var shareArray: Array<String>
 
@@ -38,6 +49,35 @@ class SettingsFragment : Fragment() {
         binding.submitEdit.setOnClickListener {
             setEditable(false)
             //TODO save edited user name/email/phone on submitEdit click
+        }
+
+        binding.appVersion.text = BuildConfig.VERSION_NAME
+        binding.deviceId.text = ""
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                binding.deviceId.text = CellWatchApp.localDataStore.getDeviceId.first()
+            } catch (t: Throwable) {
+                Log.e(TAG, "failed to set device ID")
+            }
+        }
+
+        val clipboard = context?.getSystemService(Context.CLIPBOARD_SERVICE).let {
+            if (it is ClipboardManager) {
+                it
+            } else {
+                Log.e(TAG, "expected ClipboardManager, got $it")
+                null
+            }
+        }
+
+        if (clipboard != null) {
+            fun copy(label: String, text: CharSequence) {
+                clipboard.setPrimaryClip(ClipData.newPlainText(label, text))
+                Toast.makeText(requireContext(), getString(R.string.copied, text), Toast.LENGTH_SHORT).show()
+            }
+
+            binding.deviceIdRow.setOnClickListener { copy("device ID", binding.deviceId.text) }
+            binding.appVersionRow.setOnClickListener {copy("app version", binding.appVersion.text) }
         }
 
         setEditable(false)
