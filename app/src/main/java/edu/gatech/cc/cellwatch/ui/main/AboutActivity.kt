138078f46621e7ed988e7cb8ce8b5757eb21bc36
Jason Cox <me@jasoncarloscox.com>
2024-05-22 16:32:57 -0400

    Add missing open source license info
    Use a handy library that auto-generates the required info for all Gradle
    dependencies. Manually add info for dependencies not in Gradle.

--- app/src/main/java/edu/gatech/cc/cellwatch/ui/main/AboutActivity.kt
+++ app/src/main/java/edu/gatech/cc/cellwatch/ui/main/AboutActivity.kt
@@ -1,11 +1,14 @@
 package edu.gatech.cc.cellwatch.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.core.view.allViews
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import edu.gatech.cc.cellwatch.R
 import edu.gatech.cc.cellwatch.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
@@ -19,10 +22,15 @@ class AboutActivity : AppCompatActivity() {
         binding.navDrawer.setOnCloseListener { binding.root.closeDrawer(GravityCompat.START) }
         binding.navDrawer.setActiveActivity(this)
         setSupportActionBar(binding.toolbar)
-        binding.toolbar.setNavigationOnClickListener {binding.root.openDrawer(GravityCompat.START) }
+        binding.toolbar.setNavigationOnClickListener { binding.root.openDrawer(GravityCompat.START) }
 
         binding.root.allViews
             .filterIsInstance(TextView::class.java)
             .forEach { it.movementMethod = LinkMovementMethod.getInstance() }
+
+        OssLicensesMenuActivity.setActivityTitle(getString(R.string.dep_licenses))
+        binding.showDepLicenses.setOnClickListener {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        }
     }
 }
